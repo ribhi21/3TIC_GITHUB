c646be5cbc420fa31e23fd193e732d15207ebe18
ribhi21 <ribhi15ti@mahasiswa.pcr.ac.id>
2017-10-27 07:54:18 +0700

    Telah ditambahkan satu wb form baru

---
+++ 3tic_github/GithubPertama/CobaGitHub.aspx.vb
@@ -0,0 +1,8 @@
+﻿Public Class CobaGitHub
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Label1.Text = "Selamat pagi cik gu"
+    End Sub
+
+End Class
